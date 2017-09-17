05376197664e2643b0970f468eba96c7fc974c25
shlomioz <shlomioz17@gmail.com>
2017-09-18 01:31:28 +0300

    init

---
+++ Assets/Scripts/GameLogic/WalkTo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkTo : MonoBehaviour {
+
+	public Transform goal;
+	public bool isOnFans = false;
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+		agent.speed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
