56837d27d795ff0f09437ea56ecc8f8819c27e9d
shlomioz <shlomioz17@gmail.com>
2017-09-18 15:33:17 +0300

    Fence and logic
    Fence added and logic to the zombie,
    added Bow that need to change to gun.

---
+++ Assets/Scripts/GameLogic/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy: MonoBehaviour{
+	protected int Lives;
+	protected int Power;
+	protected int Speed;
+	public bool isDead{ get; set; }
+	/*[SerializeField] private*/public Transform goal;
+	public bool isOnFans = false;
+
+
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+		agent.speed = 1f;
+	}
+
+	protected void UpdateSpeed(){
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.speed = this.Speed;
+	}
+		
+	protected void StopMovement(){
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = gameObject.transform.position;
+	}
+	/// <summary>
+	/// when the enemy got shot
+	/// </summary>
+	protected void hitted(){
+		Lives--;
+		if (Lives == 0) {
+			isDead = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		//need to check if this is the fans or the arrow
+	}
+}
---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] private Text fpsTest; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_fpsTest ();
+	}
+
+	private void _fpsTest(){
+		fpsTest.text = "FPS:" + 1 / Time.deltaTime;
+	}
+
+
+}
