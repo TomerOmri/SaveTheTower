364da38f382cbe1fdfca95306eeaaa9c395bb676
shlomioz <shlomioz17@gmail.com>
2017-09-23 19:45:23 +0300

    Menu added

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public void startNewGame(){
+		SceneManager.LoadScene (1);
+		//GameManager.Instance.SetActiveGameCanvas (true);
+	}
+
+	/*public void howToPlay(){
+		//gameObject
+	}*/
+}
---
+++ Assets/Scripts/UI/StartGameBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameBtn : MonoBehaviour {
+	const int TheGame = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider col){
+		SceneManager.LoadScene (TheGame);
+	}
+}
