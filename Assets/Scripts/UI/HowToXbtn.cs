5b5ab6be258584be9262fecb5af898938a15e44e
TomerOmri <tomer92@gmail.com>
2017-10-28 20:17:38 +0300

    Exit buttons

---
+++ Assets/Scripts/UI/HowToXbtn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToXbtn : MonoBehaviour {
+
+	public GameObject menuCanvas;
+
+	void OnTriggerEnter (Collider col){
+		gameObject.SetActive (false);
+		menuCanvas.gameObject.SetActive (true);
+	}
+}
